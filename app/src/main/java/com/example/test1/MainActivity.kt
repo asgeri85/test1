4a24ae9273129c97c9e71afe9d7d2d28df8b8c2a
asgeri85 <iasgeri456@gmail.com>
2025-02-07 20:26:54 +0400

    toast mesaj eleve olundu

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.test1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        Toast.makeText(applicationContext,"Teze kod",Toast.LENGTH_SHORT).show()
     }
 }
